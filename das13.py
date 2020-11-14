6c89d415cab2ab07eb760385e38b246d2615139a
LowerBeats <artak0753@gmail.com>
2020-11-14 20:44:00 +0400

    4RT FILE

---
+++ das13.py
@@ -0,0 +1,155 @@
+# fruits = ['banana', 'apple', 'cherry', 'kiwi']
+# print(fruits[1:3])
+# print(fruits[:3])
+# print(fruits[1:])
+# print(fruits[-3:-1])
+
+##
+
+# fruits = ['banana', 'apple', 'cherry']
+# fruits[1] = 'kiwi'
+# print(fruits)
+
+##
+
+# fruits  = ['banana', 'apple', 'cherry']
+# fruits.append('orange')
+# print(fruits)
+
+##
+
+# fruits = ['banana', 'apple', 'cherry']
+# fruits.insert(1,'orange')
+# print(fruits)
+
+##
+
+# fruits = ['banana', 'apple', 'cherry']
+# fruits.remove('apple')
+# print(fruits)
+
+##
+
+# fruits = ['banana', 'apple', 'cherry']
+# fruits.pop()
+# print(fruits)
+
+##
+
+# fruits = ['banana', 'apple', 'cherry']
+# del fruits[2]
+# del fruits
+# print(fruits)
+
+##
+
+# fruits = ['banana', 'apple', 'cherry']
+# numbers = [34,56,-456,7.56,-2.34]
+# fruits.extend(numbers)
+# print(fruits)
+
+##
+
+# numbers = [34,56,-456,7.56,-2.34]
+# numbers.sort()
+# print(numbers)
+
+##
+
+# numbers = [34,56,-456,7.56,-2.34]
+# numbers.reverse()
+# print(numbers)
+
+#####################################################
+
+# my_list = [1,4,7,14,367,-23,56]
+# print(max(my_list))
+# print(min(my_list))
+
+##>##
+
+# c = my_list[1]
+# for i in my_list:
+# 	if i > c:
+# 		c = i
+
+##<##
+
+# c = my_list[1]
+# for i in my_list:
+# 	if i < c:
+# 		c = i
+# print(c)
+
+####
+
+# my_list = [1,4,6,2,235,5,3,]
+
+# c = 0
+# count = 0
+
+# for x in my_list:
+# 	c += 1
+# 	count += x
+# print(x / c)
+
+####
+
+# name = input('Greq injvor anun: ')
+
+# my_list = ['Aram', 'Artur', 'Hayk', 'Edgar']
+
+# if name in my_list:
+# 	print('Yes')
+# else:
+# 	my_list.append(name)
+# 	print(my_list)
+
+####
+
+# my_list = [1,-4,2,6,23,14,-35]
+
+# c = []
+
+# for i in my_list:
+# 	if i  >= 0:
+# 		c.append(i)
+# print(c)
+
+####
+
+
+# my_list = [3,6,2,'hey',-3,6,-12]
+
+# nums = 0
+# strings = 0
+
+
+# for x in my_list:
+# 	if type(x) == str:
+# 		strings += 1
+# 	elif type(x) == int:
+# 		nums += 1
+# print('Nums-->',nums,'Strings-->',strings)
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
