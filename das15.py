116e7afe66f7d6bf9b7fbb80e0fa4a01e1e8958b
RubenMkrtchyan30 <rubmkrtchyan2015@mail.ru>
2020-11-14 20:31:26 +0400

    2RT FILE

---
+++ das15.py
@@ -0,0 +1,113 @@
+# set1 = {13,13,23,'11',"hello",3.4}
+
+# res = 11 in set1
+# print(res)
+
+##
+
+# thisset = {'apple','banana','cherry'}
+
+# for x in thisset:
+# 	print(x)
+
+###
+
+# mihat ban avelasnelu vaxt (add)
+# set1 = {1,3,5,2,2,5}
+# set1.add(25)
+# print(set1)
+
+#miqani hat ban avelasnelu hamar(update)
+# set2 = {4,6,2,6,63,1,2,5}
+# set1.update(set2)
+# print(set1)
+
+##
+# set1 = {1,4,6,8}
+# set1.remove(1)
+# print(set1)
+
+##
+# set1 = {1,4,6,8}
+# set1.discard(10)
+# print(set1)
+
+###
+
+# set1 = {1,4,7,9,'barev'}
+# set2 = {2,4,6,7,'barev'}
+
+# set3 = set1.union(set2)
+# print(set3)
+
+####
+
+# veracum set_i
+# lists = [1,2,3,1,2,3]
+# print(set(lists))
+#fronzeset..chi popoxvum
+
+##
+
+# set1 = {i for i in range(100)}
+# print(set1)
+
+##
+
+# set1 = {1,4,4,6,7,8}
+# set1.clear()
+# print(set1)
+
+##
+# set1 = {1,1,45,3,6}
+# set2 = set1.copy()
+# print(set1)
+
+##
+# set1 = {1,2,3}
+# set2 = {1,3,5}
+# set3 = {1,4,6}
+# print(set1.difference(set2,set3))
+#|
+# ##
+# print(set1.intersection(set2))
+
+##
+
+# sets = {'cola','pepsi','haykola'}
+
+# lists = []
+
+# for x in sets:
+# 	lists.append(x)
+# print(lists)
+
+##
+import random
+# print (random.randint(2000,9999))
+
+##
+# password = ''
+# lists2 = ['a','v','g','n','b','j','k','b','j','k','p']
+# r = str(random.randint(10,99))
+# password_num = str(random.randint(10,99))
+# for i in range(6):
+
+# 	password_choice = random.choice(lists2)
+# 	password += password_choice
+# print(r + password + password_num)
+
+###
+
+
+# mail = input('Greq dzer maily --> ')
+# nshanner = '@.'
+# if '@mail.ru' in mail or '@gmail.com' in mail:
+# 	print('YES')
+# else:
+# 	print("NO")
+
+
+
+
+
