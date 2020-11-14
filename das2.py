ec95b981aa0d32572c6e49cf940c805904b40416
RubenMkrtchyan30 <rubmkrtchyan2015@mail.ru>
2020-11-14 20:27:15 +0400

    FIRST COMMENT

---
+++ das2.py
@@ -0,0 +1,102 @@
+# name = 'Artak'
+# age = 17
+# car = 'bmw'
+# phone = 'xiaomi'
+# street = 'es tonakan'
+# # print(name,age,car,phone,street)
+
+##
+
+# 2car = "priora"
+# pho ne = 'samsung'
+# print (2car,pho ne)
+
+##
+
+# x = 5
+# y = 7.5 
+# patasxan = x + y
+# print(patasxan)
+
+##
+
+# x = 10
+# y = 4
+
+# patasxan = x > y
+# print (patasxan)
+
+##
+
+# x = 5
+# print (type (x))
+
+##
+
+'''tesak'''
+
+##
+
+# erkarutyun = int(input('Greq vorev e tiv: '))
+# laynutyun = int(input('Greq evs mek hat tiv: '))
+# makeres = erkarutyun * laynutyun
+
+# print (makeres)
+
+# r = int(input('greq vorev e tiv '))
+# p = 3.14
+
+# s = 2 * p * r * r
+
+# print (s)
+
+
+# a = int(input('Greq A tivy: '))
+# b = int(input('Greq B tivy: '))
+
+# result = (a-b)*(a+b)
+# print (result)
+
+##
+
+# a = float(input('Greq vorev e tiv: '))
+# b = float(input('Greq vorev e tiv: '))
+# c = float(input('Greq vorev e tiv: '))
+
+# result = c*c == b*b + a*a
+
+# print ('Erankyuny uni 90astijan ankyun:', result)
+
+##
+
+# anun = input('Greq dzer anuny: ')
+# tariq = int(input('Qani tarekan eq?:'))
+
+# result = (2020 - tariq)
+
+# print (result)
+
+## 5^2 + 2*5*6 - 3 = 0
+
+# D = 6*6 - 4 * 5 * -3
+# print (D)
+
+# x1 = (-6 + D ** 0.5 )/ 2 * 5
+
+# print (x1)
+
+##
+
+# import random 
+
+# x = random.randint(1,10)
+# user = int(input('Greq inch vor mi hat tiv: '))
+# result = x == user
+# print (x, result)
+
+##
+
+# user = int(input('Greq tiv: '))
+
+# result = 32 + 1.8 * user
+# print(result)
